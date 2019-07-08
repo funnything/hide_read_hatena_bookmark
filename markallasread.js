0705c7f0fb37ff2207d7a10f4ce395d534e04755
funnything <coding.while.sleep@gmail.com>
2019-07-08 14:36:43 +0900

    Add hide logic

--- markallasread.js
+++ markallasread.js
@@ -1,10 +1,35 @@
+function getNodes(xpath) {
+  var nodes = [];
+  let iter = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+  var node;
+  while (node = iter.iterateNext()) {
+    nodes.push(node);
+  }
+  return nodes;
+}
+
+function getNode(xpath, root) {
+  return document.evaluate(xpath, root, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
 function hideRead() {
-  const urls = ['https://www.publickey1.jp/blog/19/spotifykubernetes.html', 'https://www.appps.jp/332577/'];
-  console.log('urls', urls);
+  var nodes = {};
+
+  let arr = getNodes("//section[@class='entrylist-unit']//div[@class='entrylist-header']").concat(getNodes("//section[@class='entrylist-unit']//ul[contains(@class,'entrylist-item')]/li"));
+  arr.forEach(function(node) {
+    let title = getNode(".//h3[@class='entrylist-contents-title']", node);
+    let url = getNode(".//a", title).getAttribute('href');
+    nodes[url] = node;
+  });
 
   chrome.runtime.sendMessage({
-    urls: urls
+    urls: Object.keys(nodes)
   }, null, function(response) {
-    console.log('response', response);
+    Object.keys(response).forEach(function(url) {
+      if (!response[url]) {
+        let node = nodes[url];
+        node.parentNode.removeChild(node);
+      }
+    });
   });
 }
