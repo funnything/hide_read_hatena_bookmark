b9ffa83ee207e6ee6376ef6dcda4cafe60e45c32
funnything <coding.while.sleep@gmail.com>
2019-07-08 12:29:51 +0900

    Just alert

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {
-    code: 'markallasread();'
+    code: 'hide_read();'
   });
 });
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  var urls = message.urls;
-  for (var i = 0; i < urls.length; i++) {
-    var url = { url: urls[i] };
-    if (message.visited) {
-      chrome.history.deleteUrl(url);
-    } else {
-      chrome.history.addUrl(url);
-    }
-  }
-});
-
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//   var urls = message.urls;
+//   for (var i = 0; i < urls.length; i++) {
+//     var url = { url: urls[i] };
+//     if (message.visited) {
+//       chrome.history.deleteUrl(url);
+//     } else {
+//       chrome.history.addUrl(url);
+//     }
+//   }
+// });
--- markallasread.js
+++ markallasread.js
@@ -1,14 +1,13 @@
-var markallasread_visited = false;
-
-function markallasread() {
-  var urls = [];
-  var links = document.links;
-  for (var i = 0; i < links.length; i++) {
-    urls.push(links[i].href);
-  }
-  chrome.runtime.sendMessage({
-    urls: urls,
-    visited: markallasread_visited
-  });
-  markallasread_visited = !markallasread_visited;
+function hide_read() {
+  alert('okipu');
+  // var urls = [];
+  // var links = document.links;
+  // for (var i = 0; i < links.length; i++) {
+  //   urls.push(links[i].href);
+  // }
+  // chrome.runtime.sendMessage({
+  //   urls: urls,
+  //   visited: markallasread_visited
+  // });
+  // markallasread_visited = !markallasread_visited;
 }
