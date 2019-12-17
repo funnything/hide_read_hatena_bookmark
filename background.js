fc5a82f1a99d13e189e22046c690045856b67a69
funnything <coding.while.sleep@gmail.com>
2019-12-17 12:35:17 +0900

    Context menu works

--- background.js
+++ background.js
@@ -17,6 +17,7 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
   chrome.tabs.executeScript(tab.id, { code: info.menuItemId + '();' })
 });
 
+// TODO: remove
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {
     code: 'hideRead();'
@@ -25,19 +26,28 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 
 // Add identifier to message, if want to use messaging multiple purpose
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  const trav = function(urls, index, results) {
-    if (index < urls.length) {
-      chrome.history.getVisits({ url: urls[index] }, function (items) {
-        results[urls[index]] = items.length == 0;
-        trav(urls, index + 1, results);
-      });
-    } else {
-      sendResponse(results);
-    }
-  };
+  if (message.getVisits) {
+    const trav = function(urls, index, results) {
+      if (index < urls.length) {
+        chrome.history.getVisits({ url: urls[index] }, function (items) {
+          results[urls[index]] = items.length == 0;
+          trav(urls, index + 1, results);
+        });
+      } else {
+        sendResponse(results);
+      }
+    };
 
-  trav(message.urls, 0, {});
+    trav(message.urls, 0, {});
 
-  // wait async sendResponse
-  return true;
+    // wait async sendResponse
+    return true;
+  } else if (message.markAsRead) {
+    message.urls.forEach(function(url) {
+      chrome.history.addUrl({ url: url });
+    });
+  } else {
+    console.log(message);
+    throw new Error('Uknown message');
+  }
 });
--- markallasread.js
+++ markallasread.js
@@ -1,9 +1,45 @@
 function hideReadItems() {
-  console.log('hideReadItems');
+  console.log('will hideReadItems');
+  var nodes = {};
+
+  let arr = getNodes("//section[@class='entrylist-unit']//div[@class='entrylist-header']").concat(getNodes("//section[@class='entrylist-unit']//ul[contains(@class,'entrylist-item')]/li"));
+  arr.forEach(function(node) {
+    let title = getNode(".//h3[@class='entrylist-contents-title']", node);
+    let url = getNode(".//a", title).getAttribute('href');
+    nodes[url] = node;
+  });
+
+  chrome.runtime.sendMessage({
+    getVisits: true,
+    urls: Object.keys(nodes)
+  }, null, function(response) {
+    Object.keys(response).forEach(function(url) {
+      if (!response[url]) {
+        let node = nodes[url];
+        node.parentNode.removeChild(node);
+      }
+    });
+    console.log('callback hideReadItems');
+  });
+  console.log('did hideReadItems');
 }
 
 function markItemsAsRead() {
-  console.log('markItemsAsRead');
+  console.log('will markItemsAsRead');
+  var nodes = {};
+
+  let arr = getNodes("//section[@class='entrylist-unit']//div[@class='entrylist-header']").concat(getNodes("//section[@class='entrylist-unit']//ul[contains(@class,'entrylist-item')]/li"));
+  arr.forEach(function(node) {
+    let title = getNode(".//h3[@class='entrylist-contents-title']", node);
+    let url = getNode(".//a", title).getAttribute('href');
+    nodes[url] = node;
+  });
+
+  chrome.runtime.sendMessage({
+    markAsRead: true,
+    urls: Object.keys(nodes)
+  });
+  console.log('did markItemsAsRead');
 }
 
 function getNodes(xpath) {
